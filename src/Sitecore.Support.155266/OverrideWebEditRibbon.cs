b02a1b5068c0d5c957abc5ba4f564ba2ba2615ba
Ivan Sheyenko <ivs@sitecore.net>
2017-04-03 14:58:24 +0300

    patch created

---
+++ src/Sitecore.Support.155266/OverrideWebEditRibbon.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.ExperienceEditor.Pipelines.GetExperienceEditorRibbon;
+using Sitecore.Support.ExperienceEditor.Speak.Ribbon.PageExtender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Pipelines.GetExperienceEditorRibbon
+{ 
+    public class OverrideWebEditRibbon : WebEditStateProcessor
+    {
+        public override void AddControl(GetExperienceEditorRibbonArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public new void Process(GetExperienceEditorRibbonArgs args)
+        {
+            var c = args.Control as RibbonWebControl;
+            if (c != null)
+            {
+                args.Control = new RibbonWebControl
+                {
+                    State = c.State
+                };
+            }
+        }
+    }
+}
